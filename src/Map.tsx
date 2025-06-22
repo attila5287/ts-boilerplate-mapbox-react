cd1fb577027ce2c02e4121ece5a2862c2842b1e3
AttilaTurkoz <42498753+attila5287@users.noreply.github.com>
2025-06-22 00:10:21 -0600

    fixed ref types, added null checks to fix typescript error on vercel

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,22 +1,27 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./App.css";
 
 function Map() {
-  const mapRef = useRef();
-  const mapContainerRef = useRef();
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     mapboxgl.accessToken =
       "pk.eyJ1IjoiYXR0aWxhNTIiLCJhIjoiY2thOTE3N3l0MDZmczJxcjl6dzZoNDJsbiJ9.bzXjw1xzQcsIhjB_YoAuEw";
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
-    });
+    
+    if (mapContainerRef.current) {
+      mapRef.current = new mapboxgl.Map({
+        container: mapContainerRef.current,
+      });
+    }
 
     return () => {
-      mapRef.current.remove();
+      if (mapRef.current) {
+        mapRef.current.remove();
+      }
     };
   }, []);
 
